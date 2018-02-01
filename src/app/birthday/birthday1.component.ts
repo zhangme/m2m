8f5a8738cd468d5267bddc838d0ca790c0ca6ec6
Meng Zhang <mengzhang@Mengs-MacBook-Pro.local>
2018-01-31 22:19:26 -0500

    bd1

--- src/app/birthday/birthday1.component.ts
+++ src/app/birthday/birthday1.component.ts
@@ -1,6 +1,6 @@
 import { Component, AfterViewInit } from '@angular/core';
 
-declare var routeAlbum: any;
+declare var bd1Main: any;
 
 @Component({
   selector: 'birthday1-component',
@@ -10,6 +10,6 @@ declare var routeAlbum: any;
 
 export class BirthDay1Component implements AfterViewInit {
   ngAfterViewInit(){
-      routeAlbum();
+      bd1Main();
   }
 }
